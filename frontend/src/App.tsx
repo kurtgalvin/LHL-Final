a9827452b021dfc172db62b00c3c05b1d3b354f4
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-06-05 11:20:16 -0700

    Map route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Map from './views/Map';
+
+enum Routes {
+  Map,
+  Stats,
+  News
+}
+
+interface Links {
+  toMap: () => void,
+  toState: () => void,
+  toNews: () => void
+}
+
 function App() {
+  const [route, setRoute] = useState<Routes>(Routes.Map)
+
+  const links: Links = {
+    toMap: () => setRoute(Routes.Map),
+    toState: () => setRoute(Routes.Stats),
+    toNews: () => setRoute(Routes.News)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {route === Routes.Map && <Map />}
     </div>
   );
 }
---
+++ frontend/src/views/Map.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProps {
+
+}
+
+const Map: React.FC<IProps> = () => {
+  return (
+    <div>
+      Map
+    </div>
+  )
+}
