06b95c9f9a353332cddee8cd172dc88470e50cfb
Jessie Wright <jessie.wright@gmail.com>
2020-06-05 11:03:42 -0700

    get marker showing with icon

--- frontend/src/components/MapContainer.tsx
+++ frontend/src/components/MapContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '60vw',
@@ -22,19 +22,35 @@ const options = {
 }
 
 function MapContainer() {
+
+  const {isLoaded, loadError} = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY })
+  const [markers, setMarkers] = React.useState([{id: 1, lat: 49.239370, lng: -123.044590 }])
+
+  if (loadError) return <div> "Error loading maps" </div>;
+  if (!isLoaded) return <div>"Loading"</div>;
+   
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} >
+
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={12}
         options={options}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+        { markers.map(marker => (
+          <Marker 
+            key={marker.id} 
+            position= {{lat: marker.lat, lng: marker.lng}} 
+            icon= {{
+              url:'/shop_cart.svg',
+              scaledSize: new window.google.maps.Size(20,20),
+              origin: new window.google.maps.Point(0,0),
+              anchor: new window.google.maps.Point(15,15)
+            }}
+          />
+        )) }
+        
       </GoogleMap>
-    </LoadScript>
   )
 }
 
