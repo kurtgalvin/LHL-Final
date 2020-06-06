82bcb81983635852d13dfe896e20621c106ad50d
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-06-06 12:54:23 -0700

    pull in processed covid data

--- frontend/src/views/Stats.tsx
+++ frontend/src/views/Stats.tsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react'
-import { LineChart, Line } from 'recharts'
+import React from 'react';
+import { LineChart, XAxis, YAxis, Tooltip, Line } from 'recharts';
+
+import bc_data from '../data/bc_data.json'
 
 interface IProps {
 
 }
 
 const StatsView: React.FC<IProps> = () => {
+
   return (
     <div className="Stats">
-      <LineChart width={600} height={300} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <LineChart width={window.innerWidth} height={300} data={bc_data}>
+        <XAxis dataKey="date"/>
+        <YAxis/>
+        <Tooltip/>
+        <Line type="monotone" dataKey="confirmed" stroke="#8884d8" dot={false} onClick={() => console.log('here')} />
+        <Line type="monotone" dataKey="deaths" stroke="#82ca9d" dot={false} />
       </LineChart>
     </div>
   )
