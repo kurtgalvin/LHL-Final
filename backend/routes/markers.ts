c6cea7870aba23909277b995700405ae2ed6a4fc
Jessie Wright <jessie.wright@gmail.com>
2020-06-07 13:55:24 -0700

    get endpoint working

--- backend/routes/markers.ts
+++ backend/routes/markers.ts
@@ -4,18 +4,32 @@ const router  = express.Router();
 
 module.exports = (db: any) => {
 
-  router.get("/", (req: Request, res: Response) => {
+  router.get("/", (req: Request, res: any) => {
     const query = `SELECT * , (SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
                     WHERE store = stores.id and commodity=1 ORDER by update_time DESC LIMIT 1) as tp_stock,
-                    SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
+                    (SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
                     WHERE store = stores.id and commodity=2 ORDER by update_time DESC LIMIT 1) as hs_stock,
-                    SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
-                    WHERE store = stores.id and commodity=3 ORDER by update_time DESC LIMIT 1) as mask_stock,
+                    (SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
+                    WHERE store = stores.id and commodity=3 ORDER by update_time DESC LIMIT 1) as mask_stock
                     FROM stores;`
+
+    db.query(query)
+    .then((data: any) => {
+      const output: {[index: string]: string | number} = {};
+      for(const record of data.rows) {
+        output[record.id] = record; 
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({data:output}));
+    
+    })                  
+                  
+                  
   });
 
  
-// UNTESTED  MAY NOT ACTUALLY NEED
+// UNTESTED & Unfinished  -- MAY NOT ACTUALLY NEED
   router.post("/", (req: any, res: Response) => {
     
     const queryParams = [req.body.name, req.body.google_place_id, req.body.type, req.body.lat, req.body.lng];
