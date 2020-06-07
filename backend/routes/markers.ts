16c135ef650ad0b29be0d7a60187675ced462af8
Jessie Wright <jessie.wright@gmail.com>
2020-06-07 13:36:58 -0700

    query for get all markers

--- backend/routes/markers.ts
+++ backend/routes/markers.ts
@@ -5,20 +5,21 @@ const router  = express.Router();
 module.exports = (db: any) => {
 
   router.get("/", (req: Request, res: Response) => {
-  
+    const query = `SELECT * , (SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
+                    WHERE store = stores.id and commodity=1 ORDER by update_time DESC LIMIT 1) as tp_stock,
+                    SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
+                    WHERE store = stores.id and commodity=2 ORDER by update_time DESC LIMIT 1) as hs_stock,
+                    SELECT title from stock_levels join commodity_updates  on stock_levels.id = stock_level 
+                    WHERE store = stores.id and commodity=3 ORDER by update_time DESC LIMIT 1) as mask_stock,
+                    FROM stores;`
   });
 
-  // googlePlaceID VARCHAR(255),
-  // type VARCHAR(255) NOT NULL,
-  // lat DECIMAL NOT NULL,
-  // lng DECIMAL NOT NULL
-
+ 
+// UNTESTED  MAY NOT ACTUALLY NEED
   router.post("/", (req: any, res: Response) => {
     
     const queryParams = [req.body.name, req.body.google_place_id, req.body.type, req.body.lat, req.body.lng];
-    
-    console.log('--------------------------------');
-    console.log(queryParams);
+  
     db.query("INSERT INTO stores (name, google_place_id, type, lat, lng) VALUES($1, $2, $3, $4, $5) RETURNING id ", queryParams)
     .then((data: any) => 
       console.log(data.rows[0].id)
