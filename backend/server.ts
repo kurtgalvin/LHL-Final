ea902f18f107113f66734314eaf5676a1358fef7
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-08 10:12:45 -0700

    sockets set up tweets are now fetched

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,15 @@
 require('dotenv').config();
 import Express from 'express'
 import BodyParser from 'body-parser';
-
+import socketio from 'socket.io'
+import http from 'http'
+// const tweets = require ('./routes/tweets')
+import tweets from './routes/tweets'
 
 const App = Express();
 const PORT = 8080;
+const server = http.createServer(App);
+const io = socketio(server);
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -27,7 +32,10 @@ const markersRouter = require("./routes/markers");
 // mount routes
 App.use("/api/markers", markersRouter(db));
 
+//Tweet stream
+tweets(App, io)
 
-App.listen(PORT, () => {
+
+server.listen(PORT, () => {
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
--- frontend/src/views/News.tsx
+++ frontend/src/views/News.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ArticleComponent from '../components/ArticleComponent'
+import socketIOClient from "socket.io-client";
 const NewsAPI = require('newsapi');
 let newsapi = new NewsAPI(process.env.REACT_APP_new_NEWSAPI);
 
@@ -8,6 +9,7 @@ let newsapi = new NewsAPI(process.env.REACT_APP_new_NEWSAPI);
 function Tweetlist() {
   // const [items, setItems] = useState([]);
   const [articles, setArticles] = useState([]);
+  // try {
   useEffect(() => {
     newsapi.v2.topHeadlines({
       // sources: 'bbc-news',
@@ -17,19 +19,45 @@ function Tweetlist() {
       country: 'ca',
       pageSize: '5'
     }).then((response : any) => {
-      // response.articles.forEach((article) => { console.log(article.title); });
        setArticles(response.articles);
+    }).catch((err: Error) => {
+      console.log(err)
     })
-  }, [])
+
+  }, []);
+
   console.log(articles)
   return <div>
-{articles.map((article, index) => (
-        // <h1>{article['title']} {article['description']}</h1>
+    {articles.map((article, index) => (
         <ArticleComponent key={index} data={article} />
     ))}
   </div>
+
 };
 
+function Tweetlist2() {
+  const [items, setItems] = useState<any>([]);
+  useEffect(() => {
+    const socket = socketIOClient('/');
+
+    socket.on('connect', () => {
+      console.log("Socket Connected");
+      socket.on("tweets", (data : any) => {
+        console.info(data);
+        let newList = [data].concat(items.slice(0, 15));
+        setItems ({items: newList});
+      });
+    });
+    socket.on('disconnect', () => {
+      socket.off("tweets")
+      socket.removeAllListeners();
+      console.log("Socket Disconnected");
+    });
+  }, [])
+  console.log(items)
+  return <div></div>
+
+};
 
 interface IProps {
 
@@ -41,6 +69,7 @@ const NewsView: React.FC<IProps> = () => {
     <div>
       News
       <Tweetlist/>
+      <Tweetlist2/>
     </div>
   )
 }
