6990e23bfe805b76dd1212400bb25ef955335794
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-06-08 23:26:14 -0700

    toggle and sellecter buttons

--- frontend/src/views/Stats/index.tsx
+++ frontend/src/views/Stats/index.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { Paper } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Paper, Button } from '@material-ui/core'
 
 import './index.scss'
 import Canada from './Canada'
+import useToggleArray from '../../hooks/useToggleArray'
+
+const selectionsArray = [
+  {
+    title: "Confirmed",
+    value: "confirmed"
+  },
+  {
+    title: "Deaths",
+    value: "deaths"
+  },
+  {
+    title: "Recovered",
+    value: "recovered"
+  }
+]
 
 export default () => {
+  const [national, setNational] = useState<boolean>(true)
+  const [selections, toggleSelection] = useToggleArray(["confirmed"])
+
   return (
     <div className="Stats">
       <header>
-        <div className="A">A</div>
-        <div className="B">B</div>
+        <Paper className="toggle" elevation={3}>
+          <Button 
+            color="primary" 
+            variant={national ? 'contained' : 'outlined'}
+            onClick={() => setNational(true)}
+          >National</Button>
+          <Button 
+            color="primary" 
+            variant={national ? 'outlined' : 'contained'}
+            onClick={() => setNational(false)}
+          >Global</Button>
+        </Paper>
+        <Paper className="select" elevation={3}>
+          {selectionsArray.map(i => <Button 
+              color="primary" 
+              variant={selections.includes(i.value) ? 'contained' : 'outlined'}
+              onClick={() => toggleSelection(i.value)}
+            >
+              {i.title}
+            </Button>
+          )}
+        </Paper>
       </header>
-      <Canada />
+      {React.useMemo(() => <Canada />, [])}
     </div>
   )
 }
