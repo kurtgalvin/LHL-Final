d24b8388f4e5cfce4c760ecf8998185a6602ae5e
Jessie Wright <jessie.wright@gmail.com>
2020-06-10 14:02:38 -0700

    added distraction view

---
+++ frontend/src/views/Distraction.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+import { render } from 'react-dom';
+
+
+
+interface IProps {
+
+}
+
+const DistractionView: React.FC<IProps> = () => {
+  return (<div>
+    
+
+  poop
+
+
+    
+  </div> );
+   
+}
+
+export default DistractionView
