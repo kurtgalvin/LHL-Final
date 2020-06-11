e4fe64b3c62c64ed4f942a332bdb7ad2b99bf6bf
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-11 12:40:40 -0700

    finished animation of info page/added img

--- frontend/src/views/Info.tsx
+++ frontend/src/views/Info.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Paper } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import '../styles/Info.scss';
 
 interface IProps {
@@ -8,44 +8,43 @@ interface IProps {
 
 const InfoView: React.FC<IProps> = () => {
   return (
-  <div className= "Container">
-    <h1 className="Title"> Info
+  <Paper elevation={1} className= "Container">
+    <h1 className="Title"> Info You Need To Know
     </h1>
-    <div className="coughing" >
-      <img src='../../docs/coughing.png' alt='coughing' width="300" />
-      <div>
-        1. Stay home if you're sick or showing symptoms.
+    <Paper className="coughing" >
+      <img src='https://github.com/kurtgalvin/LHL-Final/blob/Stretch/Infograph/frontend/docs/coughing.png?raw=true' alt='coughing' width="300" />
+      <div className='text'>
+        Stay home if you're sick or showing any flue like symptoms.
       </div>
-    </div>
-    <div className="distancing">
-      <img src='../../docs/coughing.png' alt='distance' width="300" />
-      <div>
-        2. Physical distancing:  
+    </Paper>
+    <Paper className="distancing">
+      <img src='https://github.com/kurtgalvin/LHL-Final/blob/Stretch/Infograph/frontend/docs/social-distance.png?raw=true' alt='distance' width="300" />
+      <div className="text">
+        {/* 2. Physical distancing:   */}
         Stay at least 2 m away from others outside your home.
       </div>
-    </div>
-    <div className="Gathering">
-      <img src='../../docs/coughing.png' alt='gather' width="300" />
-      <div>
-        3. Avoid any gatherings of people on private property, at work, or in parks.
+    </Paper>
+    <Paper className="Gathering">
+      <img src='https://github.com/kurtgalvin/LHL-Final/blob/Stretch/Infograph/frontend/docs/gathering.png?raw=true' alt='gather' width="300" />
+      <div className='text'>
+        Avoid any gatherings of people on private property, at work, or in parks.
       </div>
-    </div>
-    <div className="Work">
-      <img src='../../docs/coughing.png' alt='work' width="300" />
-      <div>
-        4. Work at home if you can.
-
-        If your business or organization isn't an essential service, opens in new tab, you must ensure 2 m distancing for customers or staff. If 2 m isn't possible, you must close.
+    </Paper>
+    <Paper className="Work">
+      <img src='https://github.com/kurtgalvin/LHL-Final/blob/Stretch/Infograph/frontend/docs/Work.png?raw=true' alt='work' width="300" />
+      <div className= "text">
+        {/* 4. Work at home if you can. */}
+        If your business or organization isn't an essential service, you must ensure 2 m distancing for customers or staff. If 2 m isn't possible, you must close.
       </div>
-    </div>
-    <div className="Essential">
-      <img src='../../docs/coughing.png' alt='essential' width="300" />
-      <div>
-        5. Avoid non-essential outings.
+    </Paper>
+    <Paper className="Essential">
+      <img src='https://github.com/kurtgalvin/LHL-Final/blob/Stretch/Infograph/frontend/docs/essential.png?raw=true' alt='essential' width="300" />
+      <div className="text">
+        {/* 5. Avoid non-essential outings. */}
         Only go out for groceries, medical needs, or exercise. Exercise alone or with members of your household.
       </div>
-    </div>
-  </div>
+    </Paper>
+  </Paper>
   )
 }
 
