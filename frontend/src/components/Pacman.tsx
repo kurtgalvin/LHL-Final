f56d056ccea9d9b7379bb243d7a9b70e1d8e2018
Jessie Wright <jessie.wright@gmail.com>
2020-06-11 09:34:07 -0700

    made pacman move

--- frontend/src/components/Pacman.tsx
+++ frontend/src/components/Pacman.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useEventListener from '@use-it/event-listener'
 
 const initialMap = [ 
   [1,1,1,1,1,1,1,1,1,1,1,1,1], 
@@ -17,6 +18,50 @@ const Pacman = function() {
   
   const [map, setMap] = React.useState(initialMap);
   const [pacmanPos, setPacmanPos] = React.useState({x:6, y: 4});
+  
+  const handler = function (event: any) {
+    const tmpMap = [...map];
+    if (event.keyCode === 37){ // PACMAN MOVE LEFT
+			if ( map[pacmanPos.y][pacmanPos.x-1] !== 1){
+        tmpMap[pacmanPos.y][pacmanPos.x] = 3;
+        tmpMap[pacmanPos.y][pacmanPos.x - 1] = 5;
+        setMap(tmpMap);
+        setPacmanPos({ ...pacmanPos, x: pacmanPos.x - 1});
+        
+        
+			}
+		}
+		else if (event.keyCode === 38){ // PACMAN MOVE UP
+			if ( map[pacmanPos.y-1][pacmanPos.x] !== 1){
+        tmpMap[pacmanPos.y][pacmanPos.x] = 3;
+        tmpMap[pacmanPos.y - 1][pacmanPos.x] = 5;
+        setMap(tmpMap);
+        setPacmanPos( {...pacmanPos, y: pacmanPos.y -1 });
+				
+			}
+		}
+		else if (event.keyCode === 39){ // PACMAN MOVE RIGHT
+			if ( map[pacmanPos.y][pacmanPos.x+1] !== 1){
+				tmpMap[pacmanPos.y][pacmanPos.x] = 3;
+        tmpMap[pacmanPos.y][pacmanPos.x + 1] = 5;
+        setMap(tmpMap);
+        setPacmanPos({...pacmanPos, x:pacmanPos.x + 1});
+			}
+		}
+		else if (event.keyCode === 40){ // PACMAN MOVE DOWN
+			if ( map[pacmanPos.y+1][pacmanPos.x] !== 1){
+				tmpMap[pacmanPos.y][pacmanPos.x] = 3;
+        tmpMap[pacmanPos.y + 1][pacmanPos.x] = 5;
+				setMap(tmpMap);
+        setPacmanPos({...pacmanPos, y:pacmanPos.y + 1});
+				
+			}
+
+
+  }
+}
+
+  useEventListener('keydown', handler);
 
   return (
     <div className="pacman-game">
