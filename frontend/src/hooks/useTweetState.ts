7a541d3a83eb9e8b029dd1ee0123cb0677573a79
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-12 14:29:41 -0700

    adjusted gradient on articles, added new img for info

--- frontend/src/hooks/useTweetState.ts
+++ frontend/src/hooks/useTweetState.ts
@@ -12,7 +12,7 @@ export default (initTweets: any[]): [any[], (tweet: any | any[]) => void] => {
       result.forEach(t => filteredResult[t.id] = t)
       return Object.keys(filteredResult).sort((aID, bID) => {
         if (new Date(filteredResult[aID].created_at) > new Date(filteredResult[bID].created_at)) {
-          return -1
+          return -1 
         } return 1
       }).map(id => filteredResult[id])
     });
