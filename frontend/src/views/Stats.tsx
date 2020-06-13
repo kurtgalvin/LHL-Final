62b8e2b8a3e1ad7b08f348d66bf9c25fe4459294
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-06-12 17:08:34 -0700

    Rearrange layout

--- frontend/src/views/Stats.tsx
+++ frontend/src/views/Stats.tsx
@@ -138,6 +138,7 @@ const countries: IParam[] = [
 enum DailyRange {
   Week,
   Month,
+  ThreeMonths,
   All
 }
 
@@ -146,7 +147,7 @@ export default () => {
   const [argsSelected, toggleArg] = useToggleArray(["confirmed"])
   const [provincesSelected, toggleProvince] = useToggleArray(["AB", "BC",  "ON", "QC"])
   const [countriesSelected, toggleCountry] = useToggleArray(["Canada", "Iran", "France", "Russia"])
-  const [dailyRangeSelection, setDailyRangeSelection] = useState(DailyRange.Week)
+  const [dailyRangeSelection, setDailyRangeSelection] = useState(DailyRange.Month)
 
   const dataLastIndex = canada ? (canadaData as any)[canadaData.length - 1] : (globalData as any)[globalData.length - 1]
   const currSelected = canada ? provincesSelected : countriesSelected
@@ -160,40 +161,40 @@ export default () => {
     currData = canada ? canadaData.slice(-7) : globalData.slice(-7)
   } else if (dailyRangeSelection === DailyRange.Month) {
     currData = canada ? canadaData.slice(-30) : globalData.slice(-30)
+  } else if (dailyRangeSelection === DailyRange.ThreeMonths) {
+    currData = canada ? canadaData.slice(-90) : globalData.slice(-90)
   } else if (dailyRangeSelection === DailyRange.All) {
     currData = canada ? canadaData : globalData
   }
 
   return (
     <div className="Stats">
-      <header>
-        <Paper className="CanadaGlobalToggle" elevation={3}>
-          <Button 
-            color="primary" 
-            variant={canada ? 'contained' : 'outlined'}
-            onClick={() => setCanada(true)}
-          >Canada</Button>
-          <Button 
-            color="primary" 
-            variant={canada ? 'outlined' : 'contained'}
-            onClick={() => setCanada(false)}
-          >Global</Button>
-        </Paper>
-        <Paper className="CaseTypeSelectors" elevation={3}>
-          <div className="CaseTypeSelectors__ButtonGroup">
-            {args.map(i => <Button 
-                key={i.value}
-                color="primary" 
-                variant={argsSelected.includes(i.value) ? 'contained' : 'outlined'}
-                onClick={() => toggleArg(i.value)}
-              >
-                {i.title}
-              </Button>
-            )}
-          </div>
-          <h1>Cumulative</h1>
-        </Paper>
-      </header>
+      <Paper className="CanadaGlobalToggle" elevation={3}>
+        <Button 
+          color="primary" 
+          variant={canada ? 'contained' : 'outlined'}
+          onClick={() => setCanada(true)}
+        >Canada</Button>
+        <Button 
+          color="primary" 
+          variant={canada ? 'outlined' : 'contained'}
+          onClick={() => setCanada(false)}
+        >Global</Button>
+      </Paper>
+      <Paper className="CaseTypeSelectors" elevation={3}>
+        <div className="CaseTypeSelectors__ButtonGroup">
+          {args.map(i => <Button 
+              key={i.value}
+              color="primary" 
+              variant={argsSelected.includes(i.value) ? 'contained' : 'outlined'}
+              onClick={() => toggleArg(i.value)}
+            >
+              {i.title}
+            </Button>
+          )}
+        </div>
+        <h1>Cumulative</h1>
+      </Paper>
       
       <Paper className="ButtonGroup" elevation={3}>
         {(canada ? provinces : countries).map(pObj => {
@@ -230,6 +231,11 @@ export default () => {
             variant={dailyRangeSelection === DailyRange.Month ? 'contained' : 'outlined'}
             onClick={() => setDailyRangeSelection(DailyRange.Month)}
           >Month</Button>
+          <Button 
+            color="primary" 
+            variant={dailyRangeSelection === DailyRange.ThreeMonths ? 'contained' : 'outlined'}
+            onClick={() => setDailyRangeSelection(DailyRange.ThreeMonths)}
+          >3 Months</Button>
           <Button 
             color="primary" 
             variant={dailyRangeSelection === DailyRange.All ? 'contained' : 'outlined'}
