f08083dd690f26e06ce4a8acd546e043175b89a8
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-06-14 14:02:50 -0700

    implement cache tweets

--- backend/routes/tweets.ts
+++ backend/routes/tweets.ts
@@ -1,63 +1,72 @@
 import { userInfo } from "os";
 
-const Twitter = require('twitter');
+import Twitter from 'twitter';
+
+const EMIT_TWEET = "tweets"
+
+const twitterIDs = [
+  '724478906829426688',
+  '146569971', 
+  '141379865'
+]
 
 let twitter = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: (process.env.CONSUMER_KEY as string),
+  consumer_secret: (process.env.CONSUMER_SECRET as string),
+  access_token_key: (process.env.TWITTER_ACCESS_TOKEN_KEY as string),
+  access_token_secret: (process.env.TWITTER_ACCESS_TOKEN_SECRET as string)
 });
 
+const recentTweetsCache: any = {
+  lastUpdate: null,
+  tweets: []
+}
 
-export default (app : any, io : any) => {
-
-  let socketConnection: any;
-  const sendMessage = (msg : any) => {
-    socketConnection.emit("tweets", msg);
-  }
-
-  const twitterIDs = [
-    '724478906829426688',
-    '146569971', 
-    '141379865'
-  ]
-
-  const stream = () => {
-    const params = { follow: twitterIDs.join(',') };
-    twitter.stream('statuses/filter', params, (stream: any) => {
-      stream.on('data', (tweet: any) => {
-        if (!tweet.msg.include("RT")) {
-          sendMessage(tweet);
-        } else {
-          console.log('RT >>>', tweet.msg)
-        }
-      });
-
-      stream.on('error', (error: any) => {
-        console.log(error, "^^^ stream ERROR");
-      });
-    })
-  }
-
-  const fetchRecentTweets = () => {
+const fetchRecentTweets = (socket: any) => {
+  const delay = 15 * 60 * 1000
+  if (!recentTweetsCache.lastUpdate || recentTweetsCache.lastUpdate < Date.now() - delay) {
+    console.log("NO CACHE");
+    recentTweetsCache.tweets = [];
     twitterIDs.forEach(twitterID => {
       const params = { user_id: twitterID, include_rts: false }
       twitter.get('statuses/user_timeline', params, (error: any, tweets: any, response: any) => {
         if (!error) {
-          sendMessage(tweets.slice(0, 4));
+          recentTweetsCache.tweets = [...tweets.slice(0, 4), ...recentTweetsCache.tweets]
+          recentTweetsCache.lastUpdate = Date.now()
+          socket.emit(EMIT_TWEET, tweets.slice(0, 4))
         } else {
           console.log("fetch ERROR")
         }
       });
     })
+  } else {
+    console.log("CACHE")
+    socket.emit(EMIT_TWEET, recentTweetsCache.tweets)
   }
+}
 
+const stream = (socket: any) => {
+  const params = { follow: twitterIDs.join(',') };
+  twitter.stream('statuses/filter', params, (stream: any) => {
+    stream.on('data', (tweet: any) => {
+      if (!tweet.msg.include("RT")) {
+        socket.emit(EMIT_TWEET, tweet)
+      } else {
+        console.log('RT >>>', tweet.msg)
+      }
+    });
+
+    stream.on('error', (error: any) => {
+      console.log("^^^ stream ERROR");
+    });
+  })
+}
+
+export default (app: any, io: any) => {
   //Establishes socket connection.
-  io.on("connection", (socket : any) => {
-    socketConnection = socket;
-    fetchRecentTweets()
-    stream();
+  io.on("connection", (socket: any) => {
+    fetchRecentTweets(socket)
+    stream(socket);
     socket.on("connection", () => console.log("Client connected"));
     socket.on("disconnect", () => console.log("Client disconnected"));
   });
