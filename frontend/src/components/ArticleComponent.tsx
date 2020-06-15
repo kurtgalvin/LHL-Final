c977078ec27672fc4ff3c5506b5739ea25b0ae8d
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-15 10:29:11 -0700

    cleaned code

--- frontend/src/components/ArticleComponent.tsx
+++ frontend/src/components/ArticleComponent.tsx
@@ -5,17 +5,14 @@ import '../styles/news.scss'
 class ArticleComponent extends React.Component <{data: object}> {
   render() {
     let data : any = this.props.data;
-    if (data.images === null)
-      return null;
-    let image = data.images[0]
 
     return (
       <Paper className="Article" elevation={3}>
         <a href={data['webUrl']} target="_blank">
-          <img src= {image['url']} alt={data['images']} width="300" />
+          <img src={data.images ? data['images'][0].url : ""} alt={data['images']} width="300" />
         </a>
         <div className="overlay">
-    <div className="text">Read More</div>
+          <div className="text">Read More</div>
         </div>
         <p className="title">{data['title']}</p>
         <p className="date">{new Date(data.publishedDateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
