09483897480c4b70e0e27dbdd00205bb15f7cc06
Calvin Zheng <calvinzheng08@hotmail.com>
2020-06-14 19:02:04 -0700

    changed news api to be deployable

--- frontend/src/components/ArticleComponent.tsx
+++ frontend/src/components/ArticleComponent.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { Paper } from '@material-ui/core'
 import '../styles/news.scss'
 
-
 class ArticleComponent extends React.Component <{data: object}> {
   render() {
     let data : any = this.props.data;
+    if (data.images === null)
+      return null;
+    let image = data.images[0]
+
     return (
       <Paper className="Article" elevation={3}>
-        <a href={data['url']} target="_blank">
-          <img src={data['urlToImage']} alt={data['author']} width="300" />
+        <a href={data['webUrl']} target="_blank">
+          <img src= {image['url']} alt={data['images']} width="300" />
         </a>
         <div className="overlay">
     <div className="text">Read More</div>
         </div>
         <p className="title">{data['title']}</p>
-        <p className="date">{new Date(data.publishedAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+        <p className="date">{new Date(data.publishedDateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
       </Paper>
     )
   }
